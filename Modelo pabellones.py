d31a4999ff8afbbf032dec18c29551b7890222e4
Jgonzalezh <60612742+Jgonzalezh@users.noreply.github.com>
2020-06-15 16:30:37 -0400

    Restricciones primera versión
    Se agregan las básicas y el diccionario de turnos está funcional para todos los posibles turnos

--- Modelo pabellones.py
+++ Modelo pabellones.py
@@ -14,6 +14,27 @@ import matplotlib.colors as mcolors
 import matplotlib.transforms as mtransforms
 import matplotlib.patches as mpatches
 
+def diccionario_bloques(bloque_ini,bloque_fin):
+    diccionario=[]
+    h=bloque_ini
+    g=bloque_fin
+    for k in range(h,g):
+        for j in range (k,g):    
+            largo=[]
+            #for i in [16]:#]range(16,42):
+            for k in range(k):
+                largo.append(0)
+            for i in range(k,g):
+                if i<=j:
+                    largo.append(1)
+                else:
+                    largo.append(0)
+            for g in range(g,48):
+                largo.append(0)
+            diccionario.append(largo)
+    return diccionario
+        
+
 #should make dictionary for variables
 E_str=['EESS','COLUMNA','CPQ']
 E=[0,1,2]
@@ -31,60 +52,62 @@ dias_str=['Lunes','Martes','Miércoles','Jueves','Viernes','Sábado','Domingo']
 Tipo=[1,2,3,4,5,6,7]
 Q_horas=[77.7,85,90] #ordenadas por especialidad mínimo (vemos a que apuntamos acá)
 Q_portramo=[[14,14,7,21,0,21,7],[15,25,25,10,5,0,5,0],[25,15,25,10,5,0,0,10]]
-#Horario de funcionamiento 8:00-21:00
-diccionario_T=[[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0], #turno de 8 a 17
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0], #turno de 8:30 a 17:30
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0], #turno de 9 a 18:00
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0], #turno de 11:00 a 20:00
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0], #turno de 11:30 a 20:30
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], #turno de 9:30 a 13:30
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0], #14:00 a 18:00
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], #8:00-12:00
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], #8:30-12_30
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], #9:00-13:00
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], #9:00-15:00
-            [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0]] #12-18
-            #[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], #12-16
-            #[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], #11-15
-            #[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0], #10-14
-            #[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]] #16-20
-            
-prueba=[]
+#Horario de funcionamiento 8:00-21:->16,42
+turnos=diccionario_bloques(16,42)
+T=range(len(turnos))
 
-
-
-for j in range (16,42):    
-    for i in [16]:#]range(16,42):
-        largo=[]
-    
-        for k in range(16):
-            largo.append(0)
-        if i<=j:
-            largo.append(1)
-        else:
-            largo.append(0)
-        for g in range(42,48):
-            largo.append(0)
-        
 
 
 #Definir problema
 modelo=LpProblem('Bloques de pabellones', LpMinimize) # Se define el modelo
 #Variables
-A=LpVariable.dicts('Bloques propuestos', [(e,p,h,d)for h in index for d in dias for p in P for e in E],lowBound=0, upBound=None,cat='Binary')
+A=LpVariable.dicts('Bloques propuestos', [(e,p,t,d)for t in T for d in dias for p in P for e in E],lowBound=0, upBound=None,cat='Binary')
 
 #Función objetivo        
-modelo+=pulp.lpSum([A[(e,p,h,d)] for h in index for d in dias for p in P for e in E])
+modelo+=pulp.lpSum([A[(e,p,t,d)] for t in T for d in dias for p in P for e in E]) #minimizar N° Bloques a la semana
 #Restricciones
+
+# Solo se asigna a una especialidad al mismo tiempo el pabellón
 for  p in P:
     for d in dias:
         for h in index:
-            modelo+=pulp.lpSum([A[(e,p,h,d)] for e in E])<=1 #Solo se asigna a una especialidad al mismo tiempo el pabellón
+            modelo+=pulp.lpSum([turnos[t][h]*A[(e,p,t,d)] for e in E for t in T])<=1 
+# Se satisface la demanda semanal
 for e in E:
-    modelo+=pulp.lpSum([A[(e,p,h,d)] for h in index for d in dias for p in P])>=Q_horas[e]*2 # Se satisface la demanda semanal
+    modelo+=pulp.lpSum([turnos[t][h]*A[(e,p,t,d)] for t in T for h in index for d in dias for p in P])>=Q_horas[e]*2 
+# Bloque es continuo abordada por diccionario
+# No hay más de tres bloques por día de cada especialidad
+for d in dias:
+    for e in E:
+        modelo+=pulp.lpSum([A[(e,p,t,d)] for t in T for p in P])<=3    
+# No hay más de tres bloques en simultaneo (abordada por previa, si son numeros distintos se debe hacer)
+"""
+for d in dias:
+    for e in E:
+        for h in index:
+            modelo+=pulp.lpSum(A[(e,p,t,d)] for t in T for d in dias for p in P])<=3   
+"""
+# No se asignan más pabellones de los disponibles (ya restringidos por N de pabellones)
+# A futuro poner largo máximo de un bloque(?) Puede ser en el diccionario de turnos tb para simplificar problema
+"""
+for  p in P:
+    for d in dias:
+        for e in E:
+            modelo+=pulp.lpSum([turnos[t][h]*A[(e,p,t,d)] for t in T])<=24
+"""
+# Cantidad de bloques mínimos a la semana (ejemplo tres, pero puede ser un diccionario para cada especialidad)
+for e in E:
+    modelo+=pulp.lpSum([A[(e,p,t,d)] for t in T for p in P for d in dias])>=3  
 
-for h in index[1:-1]:
-    modelo
+# Dividiendo el instrumental se puede hacer todas las cirugías a la semana (cajas) --->FUTURA, hay que modelar los datos
+
+modelo.solve(pulp.PULP_CBC_CMD(maxSeconds=600, msg=1, fracGap=0))#pulp.GLPK()
+print(LpStatus[modelo.status])
+
+
+
+#for h in index[1:-1]:
+#    modelo
 
     
         
